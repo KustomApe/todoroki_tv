15dccdc8af57109def6b015e3a4f090b4bcd4a2a
u1and0 <e01.ando60@gmail.com>
2020-04-22 02:11:16 +0900

    Now it is ready to scrap all the datas.

--- data/spider_V3.py
+++ data/spider_V3.py
@@ -48,61 +48,68 @@ while True:
     table_nums = browser.find_elements_by_css_selector(B_TABLE)
     if len(pager) > 0 and len(table_nums) > 0:
         print('Start getting tables...')
-        for release in table_nums:
-            n = 1
-            m = str(n)
-            # テーブルタグの中の実際のデータ
-            base_tables = browser.find_element_by_xpath('/html/body/div/main/section/div/table[' + m + ']/tbody')
-            for base_table in table_nums:
-                release_date = release.find_element_by_css_selector(DATE).text
-                print(release_date)
-                tables = base_table.find_elements_by_tag_name(COLUMN)
-                print(len(tables))
-                for table in tables:
+        e = 1
+        print(e)
+        if len(table_nums) > 0:
+            for table_num in table_nums:
+                f = str(e)
+                n = 1
+                m = str(n)
+                # テーブルタグの中の実際のデータ
+                base_tables = browser.find_element_by_xpath('/html/body/div/main/section/div/table[' + m + ']/tbody')
+                for base_table in table_nums:
                     try:
+                        release_date = browser.find_element_by_xpath('/html/body/div/main/section/div/table[' + f + ']/caption').text
+                        e += 1
+                        f = str(e)
+                        tables = base_table.find_elements_by_tag_name(COLUMN)
                         print(release_date)
-                        time_release = table.find_element_by_css_selector(TIME)
-                        time_launch = time_release.find_element_by_tag_name(TAG_SPAN).text
-                        if time_launch:
-                            print('時間 : ' + time_launch)
-                        elif time_launch == ' ':
-                            time_launch = 'None'
-                            print('時間 : ' + time_launch)
-                        else:
-                            print('Error')
-                            pass
-                        name = table.find_element_by_css_selector(NAME).text
-                        if name:
-                            print('指標名 : ' + name)
-                        sub_name = table.find_element_by_css_selector(SUB_NAME).text
-                        if sub_name:
-                            print('メモ : ' + sub_name)
-                        elif sub_name == ' ':
-                            sub_name = 'None'
-                            print('メモ : ' + sub_name)
-                        else:
-                            pass
-                        important_data = table.find_elements_by_css_selector(IMPORTANT_LEVEL)
-                        if len(important_data) > 0:
-                            nums = 0
-                            for stars in important_data:
-                                filled = stars.get_attribute('alt')
-                                if filled == FILLED_STARS:
-                                    nums += 1
-                            print('重要度 : ' + str(nums))
-                        moving_average = table.find_element_by_css_selector(MOVING_AVERAGE).text
-                        print('前回ドル円変動幅 : ' + moving_average)
-                        data01 = table.find_element_by_xpath('//*/td[5]/span').text
-                        data02 = table.find_element_by_xpath('//*/td[6]/span').text
-                        data03 = table.find_element_by_xpath('//*/td[7]/span').text
-                        print('前回(改定) : ' + data01)
-                        print('予想 : ' + data02)
-                        print('結果 : ' + data03)
+                        for table in tables:
+                            try:
+                                time_release = table.find_element_by_css_selector(TIME)
+                                time_launch = time_release.find_element_by_tag_name(TAG_SPAN).text
+                                if time_launch:
+                                    print('時間 : ' + time_launch)
+                                elif time_launch == ' ':
+                                    time_launch = 'None'
+                                    print('時間 : ' + time_launch)
+                                else:
+                                    print('Error')
+                                    pass
+                                name = table.find_element_by_css_selector(NAME).text
+                                if name:
+                                    print('指標名 : ' + name)
+                                sub_name = table.find_element_by_css_selector(SUB_NAME).text
+                                if sub_name:
+                                    print('メモ : ' + sub_name)
+                                elif sub_name == ' ':
+                                    sub_name = 'None'
+                                    print('メモ : ' + sub_name)
+                                else:
+                                    pass
+                                important_data = table.find_elements_by_css_selector(IMPORTANT_LEVEL)
+                                if len(important_data) > 0:
+                                    nums = 0
+                                    for stars in important_data:
+                                        filled = stars.get_attribute('alt')
+                                        if filled == FILLED_STARS:
+                                            nums += 1
+                                    print('重要度 : ' + str(nums))
+                                moving_average = table.find_element_by_css_selector(MOVING_AVERAGE).text
+                                print('前回ドル円変動幅 : ' + moving_average)
+                                data01 = table.find_element_by_xpath('//*/td[5]/span').text
+                                data02 = table.find_element_by_xpath('//*/td[6]/span').text
+                                data03 = table.find_element_by_xpath('//*/td[7]/span').text
+                                print('前回(改定) : ' + data01)
+                                print('予想 : ' + data02)
+                                print('結果 : ' + data03)
+                            except NoSuchElementException:
+                                print('NA')
+                        print('--------------------------------------')
+                        n += 1
                     except NoSuchElementException:
-                        print('NA')
-                print('--------------------------------------')
-            n += 1
-        btn = browser.find_element_by_css_selector(PAGER_NEXT).get_attribute('href')
+                        btn = browser.find_element_by_css_selector(PAGER_NEXT).get_attribute('href')
+                        pass
         print('next url:{}'.format(btn))
         time.sleep(3)
         browser.get(btn)
